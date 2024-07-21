1ed3f2a89b5520a0135a66123414f31398c5b03a
saijayanth59 <sithlord5995@gmail.com>
2024-07-21 16:23:09 +0530

    Update email settings

--- backend/events/utils.py
+++ backend/events/utils.py
@@ -11,28 +11,35 @@ def send_mail(name, to, fr):
 
 
 def register(name):
-    subject = f"Welcome to EVENT ORIGIN {name}!"
+    subject = f"Welcome to EVENT HORIZON {name}!"
     body = f"""
         Dear {name},
 
         Thank you for registering ! We're excited to have you join our community.
 
         you'll gain access to a range of powerful tools to help you:
+
         Event Planning and Scheduling
         Event Promotion and Registration:
         Volunteer Management
         Resource Management
         
-        We're committed to making your event planning experience seamless and enjoyable. If you have any questions or need assistance, our dedicated support team is here to help at [support email].
+        We're committed to making your event planning experience seamless and enjoyable. If you have any questions or need assistance, our dedicated support team..
 
         Let's create something extraordinary together!
 
-        If you have any questions or encounter any issues, please don't hesitate to contact our support team at [support email].
+        If you have any questions or encounter any issues, please don't hesitate to contact our support team.
 
         Welcome aboard!
 
-        Best regards,
         The Quadratic Depression Team
-        EVENT ORIGIN
+        EVENT HORIZON
 """
     return (subject, body)
+
+
+def perform_create(self, serializer):
+    mail = self.request.data.get('email')
+    if mail:
+        send_mail(name=self.request.data.get(
+            'name'), to=mail, fr='register')
--- backend/users/views.py
+++ backend/users/views.py
@@ -2,16 +2,10 @@ from .serializers import StudentSerializer
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Student
-from events.utils import send_mail
 
 
 class StudentCreate(generics.CreateAPIView):
     serializer_class = StudentSerializer
-
-    def perform_create(self, serializer):
-        mail = self.request.data.get('email')
-        if mail:
-            send_mail(name=self.request.user.name, to=mail, fr='register')
 
 
 class StudentList(generics.ListAPIView):
