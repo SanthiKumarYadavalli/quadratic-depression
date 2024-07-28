4a6a5b36dd27b69aa461aaccbd3705bca97049e9
Hemanth Kumar M <hemanthkumarrgukt@gmail.com>
2024-07-28 22:43:50 +0530

    Update Event Creation Form

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable no-unused-vars */
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuthContext } from "../context/AuthContext";
 
 const HomePage = () => {
   const { authUser } = useAuthContext();
   const navigate = useNavigate();
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   //   useEffect(() => {
   //     if (!authUser) {
@@ -17,10 +18,35 @@ const HomePage = () => {
   //     return null;
   //   }
 
+  useEffect(() => {
+    const checkScreenSize = () => {
+      return window.matchMedia("(max-width: 768px)").matches;
+    };
+
+    setIsMobileMenuOpen(checkScreenSize());
+
+    const handleResize = () => {
+      setIsMobileMenuOpen(checkScreenSize());
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <div className="relative min-h-screen flex items-center justify-center">
-      <video src="/bg.mp4" autoPlay loop muted playsInline></video>
-    </div>
+    <>
+      <div className='video-container'>
+        {isMobileMenuOpen ? (
+          <div className="relative min-h-screen flex items-center justify-center">
+            <video src="/bg_mobile.mp4" autoPlay loop muted playsInline></video>
+          </div>
+        ) : (
+          <div className="relative min-h-screen flex items-center justify-center">
+            <video src="/bg.mp4" autoPlay loop muted playsInline></video>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
