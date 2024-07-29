9dfc23938e82e1818942b75a8e1e22c27b341ed1
santhikumar148 <santhikumaryadavalli23@gmail.com>
2024-07-29 10:49:16 +0530

    add build.sh for render

---
+++ backend/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# Modify this line as needed for your package manager (pip, poetry, etc.)
+pip install -r requirements.txt
+
+# # Convert static asset files
+# python manage.py collectstatic --no-input
+
+# Apply any outstanding database migrations
+python manage.py makemigrations
+python manage.py migrate
